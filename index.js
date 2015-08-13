54d3bd3675a48ab6591a6123ffca26f747c3fc24
Henry_Miller <henry@forixwebdesign.com>
2015-08-13 11:24:27 +0700

    update fix when user config router in config/default.yml

--- index.js
+++ index.js
@@ -3,20 +3,21 @@
  *
  * npm publish <tarball> [--tag <tag>] [--access <public|restricted>]
  * npm publish <folder> [--tag <tag>] [--access <public|restricted>]
- *
+ * npm publish . --tag "first-version" --access "public"
  **/
-
+var util = require('util');
 var exports = module.exports;
-var password = require('./../../../node_modules/password-hash-and-salt');
-var md5 = require('./../../../node_modules/md5');
-var extend = require('./../../../node_modules/extend');
+var password = require('password-hash-and-salt');
+var md5 = require('md5');
+var extend = require('extend');
 var slice = Array.prototype.slice;
-
 exports.password = password;
 exports.md5 = md5;
+var querystring = require('querystring');
 
+exports.router = require('auto-route/index');
 var mysql_adapter = require('mysql-adapter');
-// var util = require('util');
+
 exports.connection = mysql_adapter.connection;
 exports.AdapterConnection = mysql_adapter.AdapterConnection;
 exports.cleanupApp = mysql_adapter.cleanupApp;
@@ -28,6 +29,237 @@ exports.getRootServer = mysql_adapter.getRootServer;
 // var CONF = rootModule.exports.CONF;
 // exports.CONF = CONF;
 // rootModule.exports.defaultApp;
+
+var common = exports;
+exports.initRunningApp = function initRunningApp(express, runningApp, CONF, swig) {
+  var app_root_dir = runningApp.__dirname;
+  runningApp.set('case sensitive routing', CONF.app.caseSensitive || 0);
+
+  runningApp.formatUrl = function(routerName, params, req) {
+    var urlFormat = '';
+    var query = {}, hash = '';
+    if( Array.isArray(routerName) ) {
+      if( routerName.length == 3 ) {
+        var moduleName = routerName[0].replace(/\//g, '_');
+        var controller = routerName[1].replace(/\//g, '_');
+        var action = routerName[2];
+
+        urlFormat = util.format('/%s/%s/%s/', moduleName, controller, action);
+
+        if( Array.isArray(params) ) {
+          urlFormat += params.join('/');
+        } else if( typeof(params) == 'object' ) {
+          Object.keys(params).forEach(function(name){
+            if( name == '_query' ) {
+              query = params[name];
+            } else if( name == '_hash' ) {
+              hash = params[name];
+            } else {
+              urlFormat += name + '/' + params[name];
+            }
+          });
+        } else {
+          urlFormat += params;
+        }
+      }
+      throw new Error("Invalid param of formatUrl");
+    } else {
+      routerName = routerName.replace(/\//g, '-');
+      if( !(routerName in runningApp.routerNames) ) {
+        throw new Error( util.format('Not found router name "%s"', routerName) );
+      }
+
+      urlFormat = runningApp.routerNames[routerName].urlFormat;
+
+
+      if( typeof(params) == 'object' && !Array.isArray(params) ) {
+        Object.keys(params).forEach(function(name){
+          if( name == '_query' ) {
+            query = params[name];
+          } else if( name == '_hash' ) {
+            hash = params[name];
+          } else {
+            eval("var regx = /(:" + name.replace(/\-/g, '\\-') + ")/;");
+            if (urlFormat.match(regx)) {
+              urlFormat = urlFormat.replace(regx, params[name]);
+            } else {
+              query[name] = params[name];
+            }
+          }
+        });
+      }
+
+      params = runningApp.routerNames[routerName].params;
+      Object.keys(params).forEach(function(key) {
+        eval("var regx = /(:"+key.replace(/\-/g, '\\-')+")/;");
+        urlFormat = urlFormat.replace(regx, (params[key]['default'] || '') );
+      });
+    }
+    if( Object.keys(query).length ) {
+      query = querystring.stringify(query);
+      if( query.length ) {
+        urlFormat += '?' + query
+      }
+    }
+    if( hash.length ) {
+      urlFormat += '#' + hash;
+    }
+    return urlFormat;
+  };
+
+  runningApp.use(function(req, res, next) {
+    // https://github.com/lorenwest/node-config/wiki/Configuration-Files
+    function loadFile(type, name) {
+      type = '/' + type.replace(/^\/+|\/+$/g, '') + '/';
+      if( Array.isArray(name) ) {
+        var moduleName = '', fileName = 'index';
+        if( name.length > 1 ) {
+          fileName = name[1];
+        }
+        moduleName = name[0];
+        fileName = fileName.replace(/\.js$/, '');
+        name = util.format('%s%s%s', moduleName, type, fileName);
+      } else if( typeof(name) == 'string' ){
+        name = name.replace(/\.js/, '');
+        name = name.replace(/\-/g, '/');
+        name = name.replace(/\./, type);
+      }
+      return require( app_root_dir + '/' + name + '.js');
+    }
+
+    req.getBlock = function() {
+      var args = Array.prototype.slice.call(arguments);
+      args = args.unshift('blocks');
+      return loadFile.apply(null, args);
+    }
+
+    req.getForm = function() {
+      var args = Array.prototype.slice.call(arguments);
+      args = args.unshift('blocks/forms');
+      return loadFile.apply(null, args);
+    }
+
+    req.getController = function() {
+      var args = Array.prototype.slice.call(arguments);
+      args = args.unshift('controllers');
+      return loadFile.apply(null, args);
+    }
+
+    req.getHelper = function(name) {
+      var args = Array.prototype.slice.call(arguments);
+      args = args.unshift('helpers');
+      return loadFile.apply(null, args);
+    }
+
+    req.getView = function() {
+      var args = Array.prototype.slice.call(arguments);
+      args = args.unshift('views');
+      return loadFile.apply(null, args);
+    }
+
+    req.formatUrl = function(routerName, params) {
+      return runningApp.formatUrl.apply(runningApp, [routerName, params, req]);
+    }
+    next();
+  });
+
+  if( 'modules' in CONF ) {
+    // Loop to all module in folder webroot/lib/<module name>
+    Object.keys(CONF.modules).forEach(function(moduleName) {
+      var modConf = CONF.modules[moduleName];
+      // console.log("CONF.modules." + moduleName + " check for loading");
+      if( modConf.enable ) {
+        console.log("CONF.modules." + moduleName + " check for loading " + app_root_dir + modConf.dirname);
+        if( 'public' in modConf && modConf['public'].length ) {
+          console.log("CONF.modules." + moduleName + " check for loading static folder " + app_root_dir + modConf.dirname + modConf['public']);
+          runningApp.use( express.static(app_root_dir + modConf.dirname + modConf['public']) );
+        }
+        if( 'routers' in modConf ) {
+          var modulePrefixPath = '/' + modConf.urlpath.replace(/^\/+|\/+$/g, '') + '/';
+          Object.keys(modConf.routers).forEach(function(controllerName) {
+            var controller = modConf.routers[controllerName];
+            Object.keys(controller).forEach(function(actionName, idx) {
+              var route = controller[actionName];
+              CONF.modules[moduleName].routers[controllerName][actionName].controller = route.controller = controllerName.replace(/\-/g, '/');
+              CONF.modules[moduleName].routers[controllerName][actionName].action = route.action = actionName;
+              CONF.modules[moduleName].routers[controllerName][actionName].prefix = route.moduleName = moduleName;
+              CONF.modules[moduleName].routers[controllerName][actionName].prefix = route.prefix = modulePrefixPath;
+              if( 'params' in route ) {
+                Object.keys(route.params).forEach(function(key, idx1) {
+                  var param = route.params[key];
+                  if( 'format' in param ) {
+                    var format = new RegExp(param.format);
+                    runningApp.param(key, function(req, res, next) {
+                      if( format instanceof RegExp ) {
+                        if( !req.params[key].match(format) ) {
+                          if( 'default' in param ) {
+                            req.params[key] = param['default'];
+                          } else {
+                            if( 'error' in param ) {
+                              return next(new Error( util.format(param.error, req.params[key]) ));
+                            } else {
+                              return next(new Error( util.format('This field "%s" have value "%s" invalid with format "%s"', key, req.params[key], param.format) ));
+                            }
+                          }
+                        } // Valid format
+                        CONF.modules[moduleName].routers[controllerName][actionName].params[key].format = param.format = format;
+                      } else {
+                        return next( new Error("Can't load ") )
+                      }
+                      next();
+                    }); // End param of app
+                  } // End format
+                });
+              } // End params of route
+              // var info = common.getModuleInfo(moduleName, module);
+              route.runningApp = runningApp;
+              runningApp.routerNames[ util.format('%s.%s', controllerName, actionName) ] = route;
+              route.url = route.prefix + route.urlFormat;
+              if( 'required_auth' in route && route.required_auth ) {
+                runningApp[route.method](route.url, function checkAuth(req, res, next) {
+
+                }, common.router.routeUrlFormat(route) );
+              } else {
+                runningApp[route.method](route.url, common.router.routeUrlFormat(route) );
+              }
+            });
+          });
+        } else {
+          var moduleObj = require(app_root_dir + modConf.dirname);
+          module.exports[moduleName] = moduleObj; // urlpath
+          runningApp.use(modConf.urlpath, moduleObj); // attach to sub-route
+        }
+      }
+    });
+  }
+
+  // Handle 404
+  runningApp.use(function(req, res) {
+    runningApp.engine('twig', swig.renderFile);
+    runningApp.set('view engine', 'twig');
+    runningApp.set('views', app_root_dir + '/lib/sata/views');
+
+    res.status(400);
+    res.render('404', {title: '404: File Not Found'});
+  });
+
+  // Handle 500
+  runningApp.use(function(error, req, res, next) {
+    runningApp.engine('twig', swig.renderFile);
+    runningApp.set('view engine', 'twig');
+    runningApp.set('views', app_root_dir + '/lib/sata/views');
+    res.status(500);
+    res.render('500', {title:'500: Internal Server Error', error: error});
+  });
+
+  return runningApp;
+}
+
+
+
+
+
+
 
 
 exports.getRootModule = function getRootModule(name, _module) {
@@ -156,3 +388,38 @@ module.exports.dumpExit = function(message) {
 
 module.exports.extend = extend;
 module.exports.extendFrom = extendFrom;
+
+function ObjectManager() {
+    var managers = {};
+    return {
+        newInstance: function(name, _class) {
+
+            if( !(name in managers) ) {
+                var str = util.format("var obj = new %s();", name, _class);
+                // eval('managers['+name+'] = new ' + _class + '();';
+                eval(str);
+                managers[name] = obj;
+            }
+            return managers[name];
+        },
+        register: function(name, obj) {
+            managers[name] = obj;
+            return obj;
+        },
+        unregister: function(name) {
+            var obj = null;
+            if( name in managers ) {
+                obj = managers[name];
+                delete managers[name];
+            }
+            return obj;
+        },
+        registry: function(name) {
+            if( name in managers ) {
+                return managers[name];
+            }
+            return null;
+        }
+    }
+}
+module.exports.ObjectManager = new ObjectManager();
